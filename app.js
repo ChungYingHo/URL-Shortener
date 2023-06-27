0f413860fecdcf30a0c13aa378f8d7518db24fa3
Chung-Ying <ag.cyho@gmail.com>
2023-06-27 09:14:36 +0800

    feat: Add body-parser, mongoose, method-override package

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const app = express()
+const port = process.env || 3000
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) =>{
     res.render('index')
 })
 
 app.listen(3000, ()=>{
-    console.log(`The app is running on http://localhost:3000`)
+    console.log(`The app is running on http://localhost:${port}`)
 })
